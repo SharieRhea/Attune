2dd617c0212c8e2275506fd242e59d46e95afc89
sharie.rhea <sharierhea@gmail.com>
2022-10-26 16:13:54 -0400

    Added textView, working on LongClickListener for selecting dates.

--- app/src/main/java/com/example/attune/MainActivity.kt
+++ app/src/main/java/com/example/attune/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.attune
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
+import android.widget.TextView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mainCalendar = findViewById<CalendarView>(R.id.mainCalendar)
-        mainCalendar.dateTextAppearance
+        mainCalendar.setOnLongClickListener()
     }
+
+    private fun CalendarView.setOnLongClickListener() {
+        val mainCalendar = findViewById<CalendarView>(R.id.mainCalendar)
+        val dates = findViewById<TextView>(R.id.textView)
+        val listDates = mutableListOf<Long>()
+        listDates.add(mainCalendar.date)
+        dates.setText(R.string.listOfDates)
+    }
+
 }
