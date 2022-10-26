4743d0cfbb5f7ec66155cf1357f0707a86078d83
sharie.rhea <sharierhea@gmail.com>
2022-10-26 17:46:11 -0400

    Working on functionality for selecting dates. Trying to figure out how to make a variable string resource.

--- app/src/main/java/com/example/attune/MainActivity.kt
+++ app/src/main/java/com/example/attune/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.attune
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.TextView
-import java.util.ArrayList
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,15 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mainCalendar = findViewById<CalendarView>(R.id.mainCalendar)
-        mainCalendar.setOnLongClickListener()
-    }
-
-    private fun CalendarView.setOnLongClickListener() {
-        val mainCalendar = findViewById<CalendarView>(R.id.mainCalendar)
         val dates = findViewById<TextView>(R.id.textView)
         val listDates = mutableListOf<Long>()
-        listDates.add(mainCalendar.date)
-        dates.setText(R.string.listOfDates)
+
+        mainCalendar!!.setOnClickListener {
+            listDates.add(mainCalendar.date)
+        }
+
+        dates.setText(getString(R.string.listOfDates))
+        println(listDates.toString())
     }
+
+
 
 }
