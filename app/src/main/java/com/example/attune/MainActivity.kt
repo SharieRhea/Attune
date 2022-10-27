395790f23220741ea80ab176ff5f62666680d7f6
sharie.rhea <sharierhea@gmail.com>
2022-10-26 20:13:39 -0400

    Display current day selected functional.

--- app/src/main/java/com/example/attune/MainActivity.kt
+++ app/src/main/java/com/example/attune/MainActivity.kt
@@ -2,27 +2,29 @@ package com.example.attune
 
 import android.os.Bundle
 import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TextView
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dateTextView: TextView
+    lateinit var mainCalendar: CalendarView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainCalendar = findViewById<CalendarView>(R.id.mainCalendar)
-        val dates = findViewById<TextView>(R.id.textView)
-        val listDates = mutableListOf<Long>()
+        dateTextView = findViewById(R.id.textView)
+        mainCalendar = findViewById(R.id.mainCalendar)
 
-        mainCalendar!!.setOnClickListener {
-            listDates.add(mainCalendar.date)
-        }
+        mainCalendar.setOnDateChangeListener(
+            OnDateChangeListener {view, year, month, day ->
+                var dateFormatted = "$month $day $year"
+                dateTextView.text = dateFormatted
+            }
+        )
 
-        dates.setText(getString(R.string.listOfDates))
-        println(listDates.toString())
     }
-
-
-
 }
+
