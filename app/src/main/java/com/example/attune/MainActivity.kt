09ccc0f64fb406a767ee8fe1d3d89de14d3e4132
sharie.rhea <sharierhea@gmail.com>
2022-10-27 12:33:49 -0400

    Added documentation for code so far and 'no date selected' text for on startup.

--- app/src/main/java/com/example/attune/MainActivity.kt
+++ app/src/main/java/com/example/attune/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.attune
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.CalendarView.OnDateChangeListener
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Initialize CalendarView and TextView for MainActivity.
     lateinit var dateTextView: TextView
     lateinit var mainCalendar: CalendarView
 
@@ -15,9 +17,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set dateTextView and mainCalendar to their respective Views.
         dateTextView = findViewById(R.id.textView)
         mainCalendar = findViewById(R.id.mainCalendar)
 
+        // Set dateTextView for startup (no date selected yet).
+        dateTextView.text = getString(R.string.noDate)
+
+        // Listen for date to change in mainCalendar, if so, display the selected date.
         mainCalendar.setOnDateChangeListener(
             OnDateChangeListener {view, year, month, day ->
                 var dateFormatted = "$month $day $year"
@@ -25,6 +32,6 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
-    }
-}
+    } // End OnCreate.
+} // End MainActivity.
 
