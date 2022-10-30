efd168c0de4ad4a5123520acf4bbd84125f1b66b
sharie.rhea <sharierhea@gmail.com>
2022-10-30 16:52:55 -0400

    Implemented button to toggle ability to change period dates (not functional yet, text simply switches).

--- app/src/main/java/com/example/attune/MainActivity.kt
+++ app/src/main/java/com/example/attune/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.attune
 
 import android.content.res.Resources
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TextView
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     // Initialize CalendarView and TextView for MainActivity.
     lateinit var dateTextView: TextView
     lateinit var mainCalendar: CalendarView
+    lateinit var editDatesButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +23,39 @@ class MainActivity : AppCompatActivity() {
         // Set dateTextView and mainCalendar to their respective Views.
         dateTextView = findViewById(R.id.textView)
         mainCalendar = findViewById(R.id.mainCalendar)
+        editDatesButton = findViewById(R.id.editDates)
 
         // Set dateTextView for startup (no date selected yet).
         dateTextView.text = getString(R.string.noDate)
 
-        // Listen for date to change in mainCalendar, if so, display the selected date.
-        mainCalendar.setOnDateChangeListener(
-            OnDateChangeListener {view, year, month, day ->
-                var dateFormatted = "$month $day $year"
-                dateTextView.text = dateFormatted
+        // Set editDatesStatus.
+        var editDatesStatus = false
+
+        // Set OnClickListener for editDatesButton to switch editDatesStatus.
+        editDatesButton.setOnClickListener(
+            View.OnClickListener {
+                editDatesStatus = !editDatesStatus
+                if (editDatesStatus) {
+                    editDatesButton.text = getString(R.string.editDates_Finish)
+                }
+                else {
+                    editDatesButton.text = getString(R.string.editDates_Edit)
+                }
             }
         )
+
+        if (editDatesStatus) {
+            // Listen for date to change in mainCalendar, if so, display the selected date.
+            mainCalendar.setOnDateChangeListener(
+                OnDateChangeListener { view, year, month, day ->
+                    var dateFormatted = "$month $day $year"
+                    dateTextView.text = dateFormatted
+                }
+            )
+        }
+        else {
+            mainCalendar.isClickable = false
+        }
 
     } // End OnCreate.
 } // End MainActivity.
